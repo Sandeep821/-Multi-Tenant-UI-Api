9625f31f65bd028624cd3a2deebd000745d56389
Soni <sonis@vwcredit.com>
2018-09-07 15:58:29 -0500

    adding verbiage config data

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 //data
 var styleConfig = require("./data/style.json");
 var elementConfig = require("./data/style.json");
-var verbiageConfig = require("./data/style.json");
+var verbiageConfig = require("./data/verbiage.json");
 
 app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
@@ -14,49 +14,15 @@ app.get('/', (req, res) => res.send('DEMO API!'))
    app.get('/api/test', (req, res) =>{
     res.send({ 'express': 'Hello from data API' })
     });
-    
+
     app.get('/api/style-config/:tenantId', (req, res) =>{
         var tenantId = req.params.tenantId;
-        styleData = [{ 
-            "tenantName": "audiUsa",
-            "tenantId": "001",
-            "msg": "msg from Data API for Style config",
-            "site": {
-              "name": "audiUsa",
-              "style": {
-                "logo" : {
-                  "direction":"left"
-                },
-                "nav" : {
-                  "direction":"right"
-                }
-              }
-            }
-          },
-          { 
-            "tenantName": "bugattiUsa",
-            "tenantId": "002",
-            "msg": "msg from Data API for Style config",
-            "site": {
-              "name": "bugatti",
-              "style": {
-                "logo" : {
-                  "direction":"right"
-                },
-                "nav" : {
-                  "direction":"left"
-                }
-              }
-            }
-          }
-          ];
         let styleDataResult;
-         styleData.forEach(function(data) {
+        styleConfig.forEach(function(data) {
             if(data.tenantId === tenantId) {
                 styleDataResult = data;
             }
         });
-        
         return  res.status(200).json(styleDataResult);
     });
     
@@ -64,8 +30,15 @@ app.get('/', (req, res) => res.send('DEMO API!'))
         return  res.status(200).json(elementConfig);
     });
     
-    app.get('/api/verbiage-config', (req, res) =>{
-        return  res.status(200).json(verbiageConfig);
+    app.get('/api/verbiage-config/:tenantId', (req, res) =>{
+        var tenantId = req.params.tenantId;
+        let  verbiageDataResult;
+        verbiageConfig.forEach(function(data) {
+            if(data.tenantId === tenantId) {
+                verbiageDataResult = data;
+            }
+        });
+        return  res.status(200).json(verbiageDataResult);
     });
 
 app.listen(9000, () => console.log('Example app listening on port 9000!'))
