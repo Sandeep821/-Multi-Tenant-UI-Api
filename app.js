c823b95ab7af3d3744e9827448ceaa03509f9861
Soni <sonis@vwcredit.com>
2018-09-07 11:49:25 -0500

    adding new params in styleConfig endPoint

--- app.js
+++ app.js
@@ -11,12 +11,53 @@ app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"
 
 app.get('/', (req, res) => res.send('DEMO API!'))
 
-app.get('/api/test', (req, res) =>{
+   app.get('/api/test', (req, res) =>{
     res.send({ 'express': 'Hello from data API' })
     });
     
-    app.get('/api/style-config', (req, res) =>{
-        return  res.status(200).json(styleConfig);
+    app.get('/api/style-config/:tenantId', (req, res) =>{
+        var tenantId = req.params.tenantId;
+        styleData = [{ 
+            "tenantName": "audiUsa",
+            "tenantId": "001",
+            "msg": "msg from Data API for Style config",
+            "site": {
+              "name": "audiUsa",
+              "style": {
+                "logo" : {
+                  "direction":"left"
+                },
+                "nav" : {
+                  "direction":"right"
+                }
+              }
+            }
+          },
+          { 
+            "tenantName": "bugattiUsa",
+            "tenantId": "002",
+            "msg": "msg from Data API for Style config",
+            "site": {
+              "name": "bugatti",
+              "style": {
+                "logo" : {
+                  "direction":"right"
+                },
+                "nav" : {
+                  "direction":"left"
+                }
+              }
+            }
+          }
+          ];
+        let styleDataResult;
+         styleData.forEach(function(data) {
+            if(data.tenantId === tenantId) {
+                styleDataResult = data;
+            }
+        });
+        
+        return  res.status(200).json(styleDataResult);
     });
     
     app.get('/api/element-config', (req, res) =>{
